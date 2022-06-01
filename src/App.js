119d56c8fad99f3a82d17d6a7ddafc76d322b9b9
Rohit Khandelwal <rohitkhandelwal2001@gmail.com>
2022-06-01 16:55:27 +0530

    Fixed naviagtion url

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Footer from './components/footer';
 const App=()=>{
     return (
         <div>
-            <Route path="/">
+            <Route path="https://62974bd55068d3162c2f1f1c--radiant-biscotti-b7b816.netlify.app/">
                 <Login/>
                 <Footer/>
             </Route>
-            <Route path="/yt">
+            <Route path="https://62974bd55068d3162c2f1f1c--radiant-biscotti-b7b816.netlify.app/yt">
                 <YT/>
             </Route>
         </div>
